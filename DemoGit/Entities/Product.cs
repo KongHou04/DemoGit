26bd4bcda2a4a5fc3b61045a51a45dcb403d5ca3
MinhNhan16 <nhanptmps40527@gmail.com>
2025-05-25 15:28:37 +0700

    Filter-Product

--- DemoGit/Entities/Product.cs
+++ DemoGit/Entities/Product.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DemoGit.Entities
+﻿namespace DemoGit.Entities
 {
     class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
     }
 }
